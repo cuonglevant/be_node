21d340cae841a38e68537a8a58c8aa5bdaca070d
CuongLevant <cuonglnv.21it@vku.udn.vn>
2025-02-25 15:50:39 +0700

    changed index

--- app/models/index.js
+++ app/models/index.js
@@ -1,6 +1,14 @@
 import mongoose from "mongoose";
 import User from "./user.model.js";
 import Role from "./role.model.js";
+import League from "./league.model.js";
+import Team from "./team.model.js";
+import Player from "./player.model.js";
+import Match from "./match.model.js";
+import Nation from "./nation.model.js";
+import Category from "./category.model.js";
+import Content from "./content.model.js";
+import Media from "./media.model.js";
 
 mongoose.Promise = global.Promise;
 
@@ -8,6 +16,14 @@ const db = {
   mongoose,
   user: User,
   role: Role,
+  league: League,
+  team: Team,
+  player: Player,
+  match: Match,
+  nation: Nation,
+  category: Category,
+  content: Content,
+  media: Media,
 };
 
 export default db;
